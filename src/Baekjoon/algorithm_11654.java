cc4f676c5631bc7f0e49688a18ec4766c693ca88
lalwr <lalwrj@gmail.com>
2019-12-06 13:58:30 +0900

    백준 11654번 문제

---
+++ src/Baekjoon/algorithm_11654.java
@@ -0,0 +1,14 @@
+package Baekjoon;
+
+import java.util.Scanner;
+
+// https://www.acmicpc.net/problem/11654
+public class algorithm_11654 {
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+        String text = sc.next();
+        char v = text.charAt(0);
+        System.out.println((int)v);
+    }
+
+}
